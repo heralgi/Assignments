f03cbbf67550b20fac5737f1782e5a5b56ae9663
Murugesh_H <murugesh@monocept.com>
2026-02-27 12:03:21 +0530

    Assignment 1.

--- Assignment_1/Program.cs
+++ Assignment_1/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter a number:");
+            string input = Console.ReadLine();
+            int length = input.Length;
+            Console.WriteLine(length);
+            string output = "";
+
+            for (int i = length-1; i >= 0; i--)
+            {
+                output += input[i];
+            }
+            Console.WriteLine($"Reverse of {input} is {output}.");
         }
     }
 }
