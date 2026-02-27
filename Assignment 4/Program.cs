6bd8d9817f119875d0d037329c08cafcdfc86402
Murugesh_H <murugesh@monocept.com>
2026-02-27 13:52:51 +0530

    Assignment 4.

---
+++ Assignment 4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Assignment_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number == 1)
+            {
+                Console.WriteLine($"{number} is a prime number.");
+                return;
+            }
+
+            for (int i = 2; i < number; i++)
+            {
+                if( number % i == 0)
+                {
+                    Console.WriteLine($"{number} is not a prime number.");
+                    return;
+                }
+            }
+            Console.WriteLine($"{number} is a prime number.");
+        }
+    }
+}
