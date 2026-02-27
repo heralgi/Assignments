ee82247106181471cd61f65e179d7a94ca24d27a
Murugesh_H <murugesh@monocept.com>
2026-02-27 12:57:38 +0530

    Assignment 2.

---
+++ Assignment 2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number.");
+            string input = Console.ReadLine();
+            int length = input.Length;
+            double final = 0;
+            for (int i = 0; i < length; i++)
+            {
+                double curr = (int)Math.Pow((double)char.GetNumericValue(input[i]), (double)length);
+                final += curr;
+            }
+            if (final == Convert.ToInt32(input))
+            {
+                Console.WriteLine($"{final} is Armstrong number.");
+            }
+            else
+            {
+                Console.WriteLine($"{final} is not Armstrong number.");
+            }
+        }
+    }
+}
