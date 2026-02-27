296b23097c488e187ccc1a4f553957e62bd9cce1
Murugesh_H <murugesh@monocept.com>
2026-02-27 13:06:13 +0530

    Assignment 3.

---
+++ Assignment 3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignment_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int final = 1;
+            for (int i = number; i > 0; i--)
+            {
+                final *= i;
+            }
+
+            Console.WriteLine($"Factorial of {number} is {final}");
+        }
+    }
+}
