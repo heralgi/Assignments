6b8513e99aa65eec9de92cef1b2717a2a70edc08
Murugesh_H <murugesh@monocept.com>
2026-03-03 17:25:08 +0530

    For and Foreach loop

---
+++ For_Foreach/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace For_Foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Colors = {"Red", "Green", "Blue", "Yellow", "Pink" };
+
+            Console.WriteLine("Printing using for loop.");
+            for (int i = 0; i < Colors.Length; i++)
+            {
+                Console.WriteLine(Colors[i]);
+            }
+
+            Console.WriteLine("Printing using foreach loop.");
+            foreach (string color in Colors)
+            {
+                Console.WriteLine(color);
+            }
+        }
+    }
+}
