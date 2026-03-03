7f2330b7195e848fcb68ac4e0ab1f420a79d7aec
Murugesh_H <murugesh@monocept.com>
2026-03-03 16:04:16 +0530

    For_While

---
+++ For_While/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace For_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string my_name = "MURUGESH";
+
+            Console.WriteLine("Printing using for loop.");
+            for(int i = 0; i < my_name.Length; i++)
+            {
+                Console.Write(my_name[i]);
+            }
+
+            Console.WriteLine("\nPrinting using While loop.");
+            int count = 0;
+            while (true)
+            {
+                if (count == my_name.Length) break;
+                Console.Write(my_name[count]);
+                count++;
+            }
+        }
+    }
+}
