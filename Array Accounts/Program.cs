13b02822e4c55c107d72651eb63bdbb301cc323f
Murugesh_H <murugesh@monocept.com>
2026-03-16 12:28:24 +0530

    Array Account

--- Array Accounts/Program.cs
+++ Array Accounts/Program.cs
@@ -4,11 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Account acc = null;
+            Account[] accounts = new Account[5];
             int choice;
 
             do
             {
+                Console.WriteLine("Available Accounts:");
+                for (int i = 0; i < accounts.Length; i++)
+                {
+                    if (accounts[i] != null)
+                        Console.WriteLine("Account Number: " + accounts[i].GetAccountNumber() + ", Name: " + accounts[i].GetName());
+                }
                 Console.WriteLine("\n----- BANK MENU -----");
                 Console.WriteLine("1. Create Account");
                 Console.WriteLine("2. View Balance");
@@ -31,39 +37,74 @@
                         Console.Write("Enter Account Type (Savings/Current): ");
                         string? type = Console.ReadLine();
 
-                        acc = new Account(name, balance, type);
-
-                        Console.WriteLine("Account created successfully.");
-                        Console.WriteLine("Account Number: " + acc.GetAccountNumber());
+                        for (int i = 0;i < accounts.Length; i++)
+                        {
+                            if (accounts[i] == null)
+                            {
+                                Account account = new Account(name, balance, type);
+                                accounts[i] = account;
+                                Console.WriteLine("Account created successfully.");
+                                Console.WriteLine("Account Number: " + account.GetAccountNumber());
+                                break;
+                            }
+                            if(i >= 4) {
+                                Console.WriteLine("Reached Account Limits.");
+                            }
+                        }
                         break;
 
                     case 2:
-                        if (acc != null)
-                            Console.WriteLine("Balance: " + acc.GetBalance());
-                        else
-                            Console.WriteLine("Create account first.");
+                        Console.WriteLine("Choose Account: ");
+                        int index = 1;
+                        foreach (var account in accounts)
+                        {
+                            if (account != null)
+                                Console.WriteLine($"{index}. {account.GetAccountNumber()}");
+                            index++;
+                        }
+                        int.TryParse(Console.ReadLine(), out int choice1);
+                        if(choice != null && (choice1 < 5 && choice1 > 0))
+                        {
+                            Console.WriteLine($"Balance: {accounts[choice1 - 1].GetBalance()}");
+                        }
+
                         break;
 
                     case 3:
-                        if (acc != null)
+                        Console.WriteLine("Choose Account: ");
+                        int index1 = 1;
+                        foreach (var account in accounts)
+                        {
+                            if (account != null)
+                                Console.WriteLine($"{index1}. {account.GetAccountNumber()}");
+                            index1++;
+                        }
+                        int.TryParse(Console.ReadLine(), out int choice2);
+                        if (choice2 != null && (choice2 < 5 && choice2 > 0))
                         {
                             Console.Write("Enter amount to deposit: ");
                             double dep = Convert.ToDouble(Console.ReadLine());
-                            acc.Deposit(dep);
+                            accounts[choice2 - 1].Deposit(dep);
                         }
-                        else
-                            Console.WriteLine("Create account first.");
+
                         break;
 
                     case 4:
-                        if (acc != null)
+                        Console.WriteLine("Choose Account: ");
+                        int index2 = 1;
+                        foreach (var account in accounts)
                         {
-                            Console.Write("Enter amount to withdraw: ");
-                            double wd = Convert.ToDouble(Console.ReadLine());
-                            acc.Withdraw(wd);
+                            if (account != null)
+                                Console.WriteLine($"{index2}. {account.GetAccountNumber()}");
+                            index2++;
                         }
-                        else
-                            Console.WriteLine("Create account first.");
+                        int.TryParse(Console.ReadLine(), out int choice3);
+                        if (choice3 != null && (choice3 < 5 && choice3 > 0))
+                        {
+                            Console.Write("Enter amount to deposit: ");
+                            double dep = Convert.ToDouble(Console.ReadLine());
+                            accounts[choice3 - 1].Withdraw(dep);
+                        }
                         break;
 
                     case 5:
