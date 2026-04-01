81e9993a0c75012ad121444348986cd1aa8860b9
Murugesh_H <murugesh@monocept.com>
2026-04-01 18:38:31 +0530

    MulticastDelegate

---
+++ MulticastDelegates/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace MulticastDelegates
+{
+    internal class Program
+    {
+        public delegate void Notify(string message);
+
+        public static void SendEmail(string message)
+        {
+            Console.WriteLine("Sent to Email: " + message);
+        }
+        public static void SendSMS(string message)
+        {
+            Console.WriteLine("Sent to SMS: " + message);
+        }
+
+        public static void SendWhatsApp(string message)
+        {
+            Console.WriteLine("Sent to WhatsApp: " + message);
+        }
+        static void Main(string[] args)
+        {
+            Notify notify = null;
+
+            notify += SendEmail;
+            notify += SendSMS;
+            notify += SendWhatsApp;
+            notify("Server is down!");
+
+            notify -= SendSMS;
+            notify("Server is up.");
+        }
+    }
+}
